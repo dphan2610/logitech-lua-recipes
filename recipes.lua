84e67a66a7be00d3024b157b61be58623a628fbd
dphan2610 <dphan2610@gmail.com>
2021-08-01 19:34:19 -0400

    Update recipes.lua

--- recipes.lua
+++ recipes.lua
@@ -4,6 +4,12 @@ function OnEvent(event, arg)
   
 end
 
+function LeftClick()
+	PressMouseButton(1)
+	Sleep(100)
+	ReleaseMouseButton(1)
+end
+
 function RightClick()
 	PressMouseButton(3)
 	Sleep(100)
