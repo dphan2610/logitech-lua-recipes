a804af7b93e01196417ca2c760393b979ddcefea
dphan2610 <dphan2610@gmail.com>
2021-08-01 20:11:13 -0400

    Update left-click.lua

--- left-click.lua
+++ left-click.lua
@@ -1,21 +1,11 @@
 function OnEvent(event, arg)
 	
- 
+	left_click()
   
-end
-
-function print(str)
-	OutputLogMessage(str)
 end
 
 function left_click()
 	PressMouseButton(1)
 	Sleep(100)
 	ReleaseMouseButton(1)
-end
-
-function right_click()
-	PressMouseButton(3)
-	Sleep(100)
-	ReleaseMouseButton(3)
 end
