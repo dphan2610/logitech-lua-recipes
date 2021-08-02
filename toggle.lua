77ddaa4504f037b6b7e857457702daa995862e8f
dphan2610 <dphan2610@gmail.com>
2021-08-01 20:08:14 -0400

    Create toggle.lua

---
+++ toggle.lua
@@ -0,0 +1,21 @@
+local mouse_button_8_current_state = "off"
+local mouse_button_8_next_state = {on = "off", off = "on"}
+
+function OnEvent(event, arg)
+  
+	ClearLog()
+  
+	if (event == "MOUSE_BUTTON_RELEASED" and arg == 8) then
+		mouse_button_8_current_state = mouse_button_8_next_state[mouse_button_8_current_state]
+
+		if (mouse_button_8_current_state == "on") then
+			print("it's on!")
+		end
+    
+    if (mouse_button_8_current_state == "off") then
+			print("it's off!")
+		end
+    
+	end
+	
+end
